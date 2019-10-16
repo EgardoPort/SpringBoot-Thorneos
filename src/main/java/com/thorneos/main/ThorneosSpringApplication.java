58595b32c673343e24cbe29ddf3d511c4054e2e5
douglasBalmore <douglas.perez@proyectosfgk.org>
2019-10-15 22:24:24 -0600

    Esqueleto del proyecto - Configuraciones

---
+++ src/main/java/com/thorneos/main/ThorneosSpringApplication.java
@@ -0,0 +1,13 @@
+package com.thorneos.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThorneosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThorneosSpringApplication.class, args);
+	}
+
+}
