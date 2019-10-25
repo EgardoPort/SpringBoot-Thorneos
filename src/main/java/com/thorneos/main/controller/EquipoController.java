71fb62717a2f1722748ebeea323ac9f3f3503e84
nelson.rodasfgkss <nelson.rodasfgkss@CDS-SS-2011-015.sv.cds>
2019-10-25 08:40:45 -0600

    Commit definitivo

--- src/main/java/com/thorneos/main/controller/EquipoController.java
+++ src/main/java/com/thorneos/main/controller/EquipoController.java
@@ -12,15 +12,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.thorneos.main.entities.Deportista;
+import com.thorneos.main.entities.DetalleEquipo;
 import com.thorneos.main.entities.Equipo;
-import com.thorneos.main.entities.Torneo;
+import com.thorneos.main.repository.IDeportistaRepository;
 import com.thorneos.main.repository.IEquipoRepository;
+import com.thorneos.main.service.DetalleEquipoService;
+
 @Controller
 @RequestMapping(value = "equipo")
 public class EquipoController {
 
 	@Autowired
 	IEquipoRepository iEquipo;
+	
+	@Autowired
+	IDeportistaRepository iDeportista;
+	
+	@Autowired
+	DetalleEquipoService detalleEquipoService;
 	
 	@GetMapping("index")
 	public String index(Model model) {
@@ -62,5 +72,46 @@ public class EquipoController {
 		equipo.setNum_integrantes(num);
 		iEquipo.save(equipo);
 		return "redirect:/equipo/index";
+	}
+	
+	@GetMapping("detalle/{id}")
+	public String detalle(@PathVariable Integer id, Model model) {
+
+		Equipo equipo =  iEquipo.findById(id).get();
+		
+		List<Deportista> lista = (List<Deportista>) iDeportista.findAll();
+		model.addAttribute("deportistas", lista);
+		model.addAttribute("dEquipo", new DetalleEquipo());
+		
+		List<DetalleEquipo> list = detalleEquipoService.getDeportistaByIdEquipo(equipo.getId());
+		model.addAttribute("list", list);
+		model.addAttribute("idEquipo", equipo.getId());
+		return ("detalleEquipo/index");
+	}
+	
+	@PostMapping("guardarDe")
+	public void guardarDe(HttpServletRequest req, Model model)
+	{
+		DetalleEquipo detalleEquipo = new DetalleEquipo();
+		int id_deportista = Integer.parseInt(req.getParameter("id_deportista")); 
+		int id_equipo = Integer.parseInt(req.getParameter("id_equipo"));
+		
+		Deportista deportista= detalleEquipoService.findDeportistaById(id_deportista);
+		Equipo equipo = detalleEquipoService.findEquipoById(id_equipo);
+		
+			//Asignar valores a la entidad 
+		
+			detalleEquipo.setId_equipo(equipo);
+		
+			detalleEquipo.setId_deportista(deportista);
+			detalleEquipo.setId_equipo(equipo); 
+		
+			detalleEquipoService.save(detalleEquipo);
+	}
+	
+	@GetMapping("deleteDe")
+	public void deleteDe(HttpServletRequest req) {
+		int id = Integer.parseInt(req.getParameter("id_deportista1"));
+		detalleEquipoService.deleteById(id);
 	}
 }
--- src/main/java/com/thorneos/main/repository/IDetalleEquipoRepository.java
+++ src/main/java/com/thorneos/main/repository/IDetalleEquipoRepository.java
@@ -1,6 +1,10 @@
 package com.thorneos.main.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.thorneos.main.entities.DetalleEquipo;
@@ -8,4 +12,6 @@ import com.thorneos.main.entities.DetalleEquipo;
 @Repository
 public interface IDetalleEquipoRepository extends CrudRepository<DetalleEquipo, Integer>{
 
+	@Query("SELECT e FROM DetalleEquipo e where e.id_equipo.id = :id")
+	public List<DetalleEquipo> findById_equipo(@Param("id") Integer id);
 }
